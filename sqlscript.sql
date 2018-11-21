231855d90f559d85420aa2df4a80c538dddcad34
Rahul Ghosh <ghrahul2016@gmail.com>
2018-11-22 00:52:20 +0530

    database script

--- sqlscript.sql
+++ sqlscript.sql
@@ -1,14 +1,141 @@
-#create database test;
-/*
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 21, 2018 at 07:24 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+create database test;
 use test;
-create table users
-( userid  varchar(10)  primary key,
-   uname  varchar(20) not null unique,
-   pwd     varchar(10),
-   email   varchar(30)  unique,
-   address varchar(100),
-   phone   varchar(20)
-);
-*/
-use test;
-select * from users;
+CREATE TABLE `books` (
+  `isbn` int(10) NOT NULL,
+  `title` varchar(30) DEFAULT NULL,
+  `author` varchar(50) DEFAULT NULL,
+  `pub` varchar(10) DEFAULT NULL,
+  `cat` varchar(5) DEFAULT NULL,
+  `price` int(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`isbn`, `title`, `author`, `pub`, `cat`, `price`) VALUES
+(1, 'harry  potter', 'jk', 'dc', 'Oracl', 12),
+(1001, 'Prof. JSP', 'Wrox Team', 'Wrox', 'java', 500),
+(2045, 'VB.NET Black Book', 'Steven Holtzner', 'DreamTech', '.net', 450),
+(5007, 'Prof. ASP.NET', 'Wrox Team', 'Wrox', '.net', 675);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderitem`
+--
+
+CREATE TABLE `orderitem` (
+  `ordid` int(5) NOT NULL,
+  `isbn` int(10) NOT NULL,
+  `price` int(5) DEFAULT NULL,
+  `qty` int(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `ordid` int(5) NOT NULL,
+  `userid` int(5) DEFAULT NULL,
+  `orddate` date DEFAULT NULL,
+  `totamt` int(6) DEFAULT NULL,
+  `status` char(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`ordid`, `userid`, `orddate`, `totamt`, `status`) VALUES
+(0, 2, '2018-11-21', 500, 'A');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userid` int(5) NOT NULL,
+  `uname` varchar(20) NOT NULL,
+  `pwd` varchar(10) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userid`, `uname`, `pwd`, `email`, `address`, `phone`) VALUES
+(2, 'Rk', '12345', 'rinkalsingh400@gmail.com', 'dfdf', '9999999999'),
+(4, 'pk', '23', 'rinkalsingh0@gmail.com', 'dfdf', '9999999999');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`isbn`);
+
+--
+-- Indexes for table `orderitem`
+--
+ALTER TABLE `orderitem`
+  ADD PRIMARY KEY (`ordid`,`isbn`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`ordid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userid`),
+  ADD UNIQUE KEY `uname` (`uname`),
+  ADD UNIQUE KEY `email` (`email`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
